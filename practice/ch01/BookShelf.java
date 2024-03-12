e1c7ba3bdb7329b1cebbae3aed21fb6e6566d9e3
Narae Gwon <rnjs021031@gmail.com>
2024-03-12 10:10:12 +0900

    20240312 Iterator

--- practice/ch01/BookShelf.java
+++ practice/ch01/BookShelf.java
@@ -6,24 +6,29 @@ public class BookShelf implements Iterable<Book> {
     private Book[] books;
     private int last = 0;
 
+    // 생성자
     public BookShelf(int maxsize) {
         this.books = new Book[maxsize];
     }
 
+    // 책 한 권 꺼내오기
     public Book getBookAt(int index) {
         return books[index];
     }
 
+    // 책 한 권 꽂기
     public void appendBook(Book book) {
         this.books[last] = book;
         last++;
     }
 
+    // 책 개수 얻어오기
     public int getLength() {
         return last;
     }
 
     @Override
+    // 책꽂이 반복자를 리턴하는 메소드
     public Iterator<Book> iterator() {
         return new BookShelfIterator(this);
     }
--- practice/ch01/Main.java
+++ practice/ch01/Main.java
@@ -10,18 +10,19 @@ public class Main {
         bookShelf.appendBook(new Book("Cinderella"));
         bookShelf.appendBook(new Book("Daddy-Long-Legs"));
 
-        // 명시적으로 Iterator를 사용하는 방법 
+        // Iterator 패턴
+        // 1. 책꽂이한테 책꽂이 반복자를 요청해서 얻음
         Iterator<Book> it = bookShelf.iterator();
+
+        // 2. 책꽂이 반복자를 이용해서 책을 한 권씩 꺼내 옴
         while (it.hasNext()) {
             Book book = it.next();
             System.out.println(book.getName());
         }
-        System.out.println();
 
-        // 확장 for문을 사용하는 방법 
-        for (Book book: bookShelf) {
+        // 확장 for문을 사용하는 방법
+        for (Book book : bookShelf) {
             System.out.println(book.getName());
         }
-        System.out.println();
     }
 }
