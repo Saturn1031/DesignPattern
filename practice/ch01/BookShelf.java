7e76cc7b51ad183f214a1c2876f0018c2ffd711a
Narae Gwon <rnjs021031@gmail.com>
2024-03-14 11:04:22 +0900

    20240314 배열을 ArrayList로 수정

--- practice/ch01/BookShelf.java
+++ practice/ch01/BookShelf.java
@@ -1,30 +1,36 @@
 package practice.ch01;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class BookShelf implements Iterable<Book> {
-    private Book[] books;
-    private int last = 0;
+    // private Book[] books;
+    private List<Book> books;
+    // private int last = 0;
 
     // 생성자
-    public BookShelf(int maxsize) {
-        this.books = new Book[maxsize];
+    public BookShelf(int initialsize) {
+        // this.books = new Book[maxsize];
+        this.books = new ArrayList<>(initialsize);
     }
 
     // 책 한 권 꺼내오기
     public Book getBookAt(int index) {
-        return books[index];
+        // return books[index];
+        return books.get(index);
     }
 
     // 책 한 권 꽂기
     public void appendBook(Book book) {
-        this.books[last] = book;
-        last++;
+        // this.books[last] = book;
+        books.add(book);
+        // last++;
     }
 
     // 책 개수 얻어오기
     public int getLength() {
-        return last;
+        return books.size();
     }
 
     @Override
