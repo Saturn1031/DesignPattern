78860bc2f3c39586e63726e40ec34dd58e9f9e10
Narae Gwon <rnjs021031@gmail.com>
2024-03-27 12:53:06 +0900

    20240327 ch03 hw 수정

--- hw/ch03/Main.java
+++ hw/ch03/Main.java
@@ -2,6 +2,9 @@ package hw.ch03;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("학번: 20220878 이름: 권나래");
+        System.out.println();
+        
         AbstractDisplay d1 = new KwonNaraeDisplay("권나래", "20220878", 3); // 권나래: 객체 생성 부분 수정
 
         d1.display(10);
