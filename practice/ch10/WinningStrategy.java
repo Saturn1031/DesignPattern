e50112fd5029efb2110a92858760c19668d0fe59
Narae Gwon <rnjs021031@gmail.com>
2024-04-04 11:44:29 +0900

    20240404 ch10 strategy

---
+++ practice/ch10/WinningStrategy.java
@@ -0,0 +1,26 @@
+package practice.ch10;
+
+import java.util.Random;
+
+public class WinningStrategy implements Strategy {
+    private Random random; // 난수 생성기
+    private boolean won = false; // 직전 승부
+    private Hand prevHand; // 직전에 내민 손
+
+    public WinningStrategy(int seed) {
+        random = new Random(seed);
+    }
+
+    @Override
+    public Hand nextHand() { // 가위바위보 전략의 핵심
+        if (!won) { // 직전에 졌으면...
+            prevHand = Hand.getHand(random.nextInt(3)); // 새로운 손을 얻음
+        }
+        return prevHand;
+    }
+
+    @Override
+    public void study(boolean win) {
+        won = win;
+    }
+}
