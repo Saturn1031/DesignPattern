06dbaea8f1662c62db313c588734f88c78d2fa83
Narae Gwon <rnjs021031@gmail.com>
2024-05-23 11:44:33 +0900

    20240523 ch07 builder

--- practice/ch07/Main.java
+++ practice/ch07/Main.java
@@ -1,5 +1,7 @@
 package practice.ch07;
 
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
         // TextBuilder 이용
@@ -7,5 +9,19 @@ public class Main {
         Director d1 = new Director(b1);
         d1.construct();
         b1.getTextResult();
+
+        // HTMLBuilder 이용
+        HTMLBuilder hb1 = new HTMLBuilder();
+        Director d2 = new Director(hb1);
+        d2.construct();
+        System.out.println(hb1.getHTMLResult() + "is created...");
+
+        // FrameBuilder 이용
+        FrameBuilder fb1 = new FrameBuilder();
+        Director d3 = new Director(fb1);
+        d3.construct();
+        fb1.getFrameResult();
+        JFrame frame = fb1.getFrameResult();
+        frame.setVisible(true);
     }
 }
